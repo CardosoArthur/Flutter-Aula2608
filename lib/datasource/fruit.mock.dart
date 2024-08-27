61c73746e9a36a5411795fd9920ede29d08d0994
CardosoArthur <arthurmcardoso99@gmail.com>
2024-08-26 21:04:14 -0300

    Subindo os aequivos feitos em aula

---
+++ lib/datasource/fruit.mock.dart
@@ -0,0 +1,12 @@
+import 'package:fruitstore/domain/fruit.dart';
+
+final fruitmock =[
+
+  Fruit("Laranja", "R\$ 10,00"),
+  Fruit("Laranja2", "R\$ 11,00"),
+  Fruit("Laranja3", "R\$ 12,00"),
+  Fruit("Laranja4", "R\$ 13,00"),
+  Fruit("Laranja5", "R\$ 14,00")
+
+
+];
---
+++ lib/domain/fruit.dart
@@ -0,0 +1,12 @@
+class Fruit {
+
+  String name;
+  String price;
+
+  //comando ALT+INSERT = criar construtor
+  Fruit(this.name, this.price);
+
+
+
+
+}
